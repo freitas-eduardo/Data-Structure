c7ae2892f99b19540e89cbd5b6dde438551a50f9
eduardo freitas <eduardo.g.freitas@globalhitss.com.br>
2022-03-19 15:50:04 -0300

    create priority queue

--- priorityQueue.js
+++ priorityQueue.js
@@ -2,13 +2,41 @@
 const { createQueue } = require('./queues')
 
 function createPriorityQueue() {
-    onst lowPriorityQueue = createQueue()
   const highPriorityQueue = createQueue()
+  const lowPriorityQueue = createQueue()
+
   return {
-    // enqueue 
-    // dequeue 
-    // peek 
-    // length 
-    // isEmpty
+    enqueue(item, isHighPriority = false) {
+      const queue = isHighPriority ? highPriorityQueue : lowPriorityQueue
+      queue.enqueue(item)
+    },
+    dequeue() {
+      if (!highPriorityQueue.isEmpty()) {
+        return highPriorityQueue.dequeue()
+      }
+
+      return lowPriorityQueue.dequeue()
+    },
+    peek() {
+      if (!highPriorityQueue.isEmpty()) {
+        return highPriorityQueue.peek()
+      }
+
+      return lowPriorityQueue.peek()
+    },
+    get length() {
+      return highPriorityQueue.length + lowPriorityQueue.length
+    },
+    isEmpty() {
+      return highPriorityQueue.isEmpty() && lowPriorityQueue.isEmpty()
+    }
   }
 }
+
+const q = createPriorityQueue()
+
+q.enqueue('A fix here')
+
+console.log(q.peek())
+
+exports.createPriorityQueue = createPriorityQueue
