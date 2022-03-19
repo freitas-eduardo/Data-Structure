a5d9c474379b2db1413274c12b24e56ace572175
eduardo freitas <eduardo.g.freitas@globalhitss.com.br>
2022-03-19 15:30:12 -0300

    create priority queue

---
+++ priorityQueue.js
@@ -0,0 +1,14 @@
+
+const { createQueue } = require('./queues')
+
+function createPriorityQueue() {
+    onst lowPriorityQueue = createQueue()
+  const highPriorityQueue = createQueue()
+  return {
+    // enqueue 
+    // dequeue 
+    // peek 
+    // length 
+    // isEmpty
+  }
+}
